d04b4cf6322fc1707b558f05c599bdf55253cf65
ronaldo <ronaldo.dsantos@gmail.com>
2024-09-21 18:58:45 -0300

    Moedas

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+Console.Clear();
+
+decimal valor = 10536.75m; // Tipo decimal é o mais indicado para trabalhar com moedas
+
+Console.WriteLine(valor.ToString(
+  "C", // "N" para números e "C" para moedas 
+  CultureInfo.CreateSpecificCulture("pt-BR"))); // Formatando valor de acordo com a moeda local
+
+Console.WriteLine(Math.Round(valor)); // Math.Round para arredondar valor de acordo com a casa decimal
+
+Console.WriteLine(Math.Ceiling(valor)); // Math.Ceilin para arredondar valor para cima
+
+Console.WriteLine(Math.Floor(valor)); // Math.Floor para arredondar valor para baixo
---
+++ Readme.md
@@ -0,0 +1,1 @@
+## Material de estudos para manipulação de moedas no C#
